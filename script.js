cfbaa783094140160cd8698dac1cd3dd3afb29ee
Ssosiashvili <sosiashvilisoso@gmail.com>
2021-07-11 00:09:38 +0400

    all tasks done

---
+++ script.js
@@ -0,0 +1,59 @@
+//2.
+// numbers
+let x = 13;
+let y = 10.50;
+
+// strings
+let firstName = "Soso";
+let lastName = 'Sosiashvili';
+
+// booleans
+let isAdult = true;
+let isReallySummer = false;
+
+// null
+let annualCost = null;
+
+//undefined
+let annualRevenue = undefined; 
+
+// 3.
+let radius = prompt ("radius?")
+let pi = 3.14159;
+let area = pi*radius*radius;
+let Circumference = 2*pi*radius ;
+
+
+console.log ("radius is " + radius)
+console.log ("Area of Circle is " + area);
+console.log ("Circumference of Circle is " + Circumference);
+
+
+// 3.
+let R = 10;
+let S = Math.PI * R * R;
+let L = 2 * Math.PI * R;
+
+
+console.log ("radius is " + R);
+console.log ("Area of Circle is " + S);
+console.log ("Circumference of Circle is " + L);
+
+
+// 4.
+let a = 5;
+let b = 12;
+
+let c = Math.sqrt(a * a + b * b);
+console.log ("third side of triangle is " + c);
+
+// 5.
+let z = prompt ("enter cube edge length");
+
+let v = z * z * z;
+console.log ("The volume of the cube is " + v);
+
+// 5.
+let edge = prompt ("enter cube edge length");
+
+console.log ("The volume of the cube is " + Math.pow(z, 3));
